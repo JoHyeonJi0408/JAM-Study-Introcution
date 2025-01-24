671c9ae1e4dfdce72d104fa7047f73838d452b11
조현지 <1692074@hansung.ac.kr>
2025-01-24 14:15:20 +0900

    캘린더 다크 모드 수정

--- src/app/components/main-client.js
+++ src/app/components/main-client.js
@@ -120,7 +120,7 @@ function MonthPicker({ selectedMonth, setSelectedMonth, uniqueMonths }) {
     };
 
     return (
-        <div className="flex flex-col items-center space-y-4 relative">
+        <div className="flex flex-col items-center space-y-4 relative dark:text-white">
             {/* Header Navigation */}
             <div className="flex items-center space-x-4">
                 <button onClick={handlePrevYear} className="px-2 py-1 border rounded">
@@ -153,6 +153,7 @@ function MonthPicker({ selectedMonth, setSelectedMonth, uniqueMonths }) {
                         value={new Date(Number(year), Number(month) - 1)}
                         onClickMonth={handleMonthChange}
                         defaultView="year"
+                        minDetail="decade"
                         tileDisabled={({ date }) => {
                             const formattedMonth = `${date.getFullYear()}년 ${String(
                                 date.getMonth() + 1
@@ -165,7 +166,7 @@ function MonthPicker({ selectedMonth, setSelectedMonth, uniqueMonths }) {
                     />
                     <button
                         onClick={() => setShowCalendar(false)}
-                        className="mt-2 px-4 py-2 bg-gray-200 dark:bg-slate-600 dark:text-slate-400 rounded"
+                        className="mt-2 px-4 py-2 bg-gray-200 dark:bg-slate-700 dark:text-slate-400 rounded"
                     >
                         닫기
                     </button>
