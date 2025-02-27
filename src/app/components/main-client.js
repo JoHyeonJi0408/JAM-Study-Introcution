feecddf21db022a3a48076ca45b4a9589a4d00b3
조현지 <1692074@hansung.ac.kr>
2025-02-27 18:06:50 +0900

    모달 버튼 다크 모드 hover 색상 추가

--- src/app/components/main-client.js
+++ src/app/components/main-client.js
@@ -320,7 +320,7 @@ export default function MainClient({ memberData }) {
                                             {portfolio && (
                                                 <a
                                                     href={portfolio}
-                                                    className="flex items-center px-4 py-2 bg-rose-400 dark:bg-rose-300 text-white dark:text-zinc-900 rounded-xl hover:bg-rose-500 transition"
+                                                    className="flex items-center px-4 py-2 bg-rose-400 dark:bg-rose-300 text-white dark:text-zinc-900 rounded-xl hover:bg-rose-500 dark:hover:bg-rose-400 transition"
                                                 >
                                                     💼 <span className="ml-2"><b>포트폴리오</b></span>
                                                 </a>
@@ -328,7 +328,7 @@ export default function MainClient({ memberData }) {
                                             {blog && (
                                                 <a
                                                     href={blog}
-                                                    className="flex items-center px-4 py-2 bg-lime-400 dark:bg-lime-300 text-white dark:text-zinc-900 rounded-xl hover:bg-lime-500 transition"
+                                                    className="flex items-center px-4 py-2 bg-lime-400 dark:bg-lime-300 text-white dark:text-zinc-900 rounded-xl hover:bg-lime-500 dark:hover:bg-lime-400 transition"
                                                 >
                                                     ✏️ <span className="ml-2"><b>블로그</b></span>
                                                 </a>
@@ -336,7 +336,7 @@ export default function MainClient({ memberData }) {
                                             {gitHub && (
                                                 <a
                                                     href={gitHub}
-                                                    className="flex items-center px-4 py-2 bg-sky-400 dark:bg-sky-300 text-white dark:text-zinc-900 rounded-xl hover:bg-sky-500 transition"
+                                                    className="flex items-center px-4 py-2 bg-sky-400 dark:bg-sky-300 text-white dark:text-zinc-900 rounded-xl hover:bg-sky-500 dark:hover:bg-sky-400 transition"
                                                 >
                                                     🐙 <span className="ml-2"><b>GitHub</b></span>
                                                 </a>
